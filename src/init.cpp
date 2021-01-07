bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

net: Add flags for port mapping protocols

src/init.cpp
@@ -1766,9 +1766,7 @@ bool AppInitMain(NodeContext& node)
     Discover();
 
     // Map ports with UPnP
-    if (gArgs.GetBoolArg("-upnp", DEFAULT_UPNP)) {
-        StartMapPort();
-    }
+    StartMapPort(gArgs.GetBoolArg("-upnp", DEFAULT_UPNP));
 
     CConnman::Options connOptions;
     connOptions.nLocalServices = nLocalServices;

src/mapport.h
@@ -12,7 +12,12 @@ static const bool DEFAULT_UPNP = USE_UPNP;
 static const bool DEFAULT_UPNP = false;
 #endif
 
-void StartMapPort();
+enum MapPortProto : unsigned int {
+    NONE = 0x00,
+    UPNP = 0x01,
+};
+
+void StartMapPort(bool use_upnp);
 void InterruptMapPort();
 void StopMapPort();
 

Kino1994/bitcoin-svn-git-history

src/init.cpp
@@ -1900,9 +1900,7 @@ bool AppInitMain(const util::Ref& context, NodeContext& node, interfaces::BlockA
     Discover();
 
     // Map ports with UPnP
-    if (args.GetBoolArg("-upnp", DEFAULT_UPNP)) {
-        StartMapPort();
-    }
+    StartMapPort(args.GetBoolArg("-upnp", DEFAULT_UPNP));
 
     CConnman::Options connOptions;
     connOptions.nLocalServices = nLocalServices;

src/mapport.h
@@ -12,7 +12,12 @@ static const bool DEFAULT_UPNP = USE_UPNP;
 static const bool DEFAULT_UPNP = false;
 #endif
 
-void StartMapPort();
+enum MapPortProtoFlag : unsigned int {
+    NONE = 0x00,
+    UPNP = 0x01,
+};
+
+void StartMapPort(bool use_upnp);
 void InterruptMapPort();
 void StopMapPort();
 