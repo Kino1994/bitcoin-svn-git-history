bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

Remove all #include // for * comments

src/init.cpp
@@ -24,8 +24,8 @@
 #include <index/blockfilterindex.h>
 #include <index/coinstatsindex.h>
 #include <index/txindex.h>
-#include <init/caches.h> // for CalculateCacheSizes
-#include <init/chainstate.h> // for LoadChainstate
+#include <init/caches.h>
+#include <init/chainstate.h>
 #include <init/common.h>
 #include <interfaces/chain.h>
 #include <interfaces/init.h>

src/test/util/setup_common.cpp
@@ -12,13 +12,13 @@
 #include <consensus/validation.h>
 #include <crypto/sha256.h>
 #include <init.h>
-#include <init/chainstate.h> // for LoadChainstate
+#include <init/chainstate.h>
 #include <interfaces/chain.h>
 #include <miner.h>
 #include <net.h>
 #include <net_processing.h>
 #include <noui.h>
-#include <node/blockstorage.h> // for fReindex, fPruneMode
+#include <node/blockstorage.h>
 #include <policy/fees.h>
 #include <pow.h>
 #include <rpc/blockchain.h>
@@ -26,7 +26,7 @@
 #include <rpc/server.h>
 #include <scheduler.h>
 #include <script/sigcache.h>
-#include <shutdown.h> // for ShutdownRequested
+#include <shutdown.h>
 #include <streams.h>
 #include <txdb.h>
 #include <util/strencodings.h>

Kino1994/bitcoin-svn-git-history

src/init.cpp
@@ -34,8 +34,8 @@
 #include <net_processing.h>
 #include <netbase.h>
 #include <node/blockstorage.h>
-#include <node/caches.h> // for CalculateCacheSizes
-#include <node/chainstate.h> // for LoadChainstate
+#include <node/caches.h>
+#include <node/chainstate.h>
 #include <node/context.h>
 #include <node/miner.h>
 #include <node/ui_interface.h>

src/test/util/setup_common.cpp
@@ -17,8 +17,8 @@
 #include <net_processing.h>
 #include <node/miner.h>
 #include <noui.h>
-#include <node/blockstorage.h> // for fReindex, fPruneMode
-#include <node/chainstate.h> // for LoadChainstate
+#include <node/blockstorage.h>
+#include <node/chainstate.h>
 #include <policy/fees.h>
 #include <pow.h>
 #include <rpc/blockchain.h>
@@ -26,7 +26,7 @@
 #include <rpc/server.h>
 #include <scheduler.h>
 #include <script/sigcache.h>
-#include <shutdown.h> // for ShutdownRequested
+#include <shutdown.h>
 #include <streams.h>
 #include <txdb.h>
 #include <util/strencodings.h>