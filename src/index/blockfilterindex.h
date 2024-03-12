furszy/bitcoin-core

index: blockfilter, decouple header lookup into its own function

src/index/blockfilterindex.h
@@ -44,6 +44,8 @@ private:
 
     bool Write(const BlockFilter& filter, uint32_t block_height, const uint256& header);
 
+    std::optional<uint256> ReadHeader(int height, const uint256& expected_block_hash);
+
 protected:
     bool CustomInit(const std::optional<interfaces::BlockKey>& block) override;
 

Kino1994/bitcoin-svn-git-history

src/index/blockfilterindex.h
@@ -46,6 +46,8 @@ private:
 
     bool Write(const BlockFilter& filter, uint32_t block_height, const uint256& filter_header);
 
+    std::optional<uint256> ReadFilterHeader(int height, const uint256& expected_block_hash);
+
 protected:
     bool CustomInit(const std::optional<interfaces::BlockKey>& block) override;
 