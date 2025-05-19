bitcoin/bitcoin

validation: make CCheckQueueControl's CCheckQueue non-optional
This simplifies the construction logic and will allow the constructor and
destructor to lock and unlock uncondiationally.

src/checkqueue.h
@@ -208,43 +208,35 @@ template <typename T, typename R = std::remove_cvref_t<decltype(std::declval<T>(
 class CCheckQueueControl
 {
 private:
-    CCheckQueue<T, R> * const pqueue;
+    CCheckQueue<T, R>& pqueue;
     bool fDone;
 
 public:
     CCheckQueueControl() = delete;
     CCheckQueueControl(const CCheckQueueControl&) = delete;
     CCheckQueueControl& operator=(const CCheckQueueControl&) = delete;
-    explicit CCheckQueueControl(CCheckQueue<T> * const pqueueIn) : pqueue(pqueueIn), fDone(false)
+    explicit CCheckQueueControl(CCheckQueue<T>& pqueueIn) : pqueue(pqueueIn), fDone(false)
     {
-        // passed queue is supposed to be unused, or nullptr
-        if (pqueue != nullptr) {
-            ENTER_CRITICAL_SECTION(pqueue->m_control_mutex);
-        }
+        ENTER_CRITICAL_SECTION(pqueue.m_control_mutex);
     }
 
     std::optional<R> Complete()
     {
-        if (pqueue == nullptr) return std::nullopt;
-        auto ret = pqueue->Complete();
+        auto ret = pqueue.Complete();
         fDone = true;
         return ret;
     }
 
     void Add(std::vector<T>&& vChecks)
     {
-        if (pqueue != nullptr) {
-            pqueue->Add(std::move(vChecks));
-        }
+        pqueue.Add(std::move(vChecks));
     }
 
     ~CCheckQueueControl()
     {
         if (!fDone)
             Complete();
-        if (pqueue != nullptr) {
-            LEAVE_CRITICAL_SECTION(pqueue->m_control_mutex);
-        }
+        LEAVE_CRITICAL_SECTION(pqueue.m_control_mutex);
     }
 };
 

Kino1994/bitcoin-svn-git-history

src/checkqueue.h
@@ -208,43 +208,35 @@ template <typename T, typename R = std::remove_cvref_t<decltype(std::declval<T>(
 class CCheckQueueControl
 {
 private:
-    CCheckQueue<T, R> * const pqueue;
+    CCheckQueue<T, R>& m_queue;
     bool fDone;
 
 public:
     CCheckQueueControl() = delete;
     CCheckQueueControl(const CCheckQueueControl&) = delete;
     CCheckQueueControl& operator=(const CCheckQueueControl&) = delete;
-    explicit CCheckQueueControl(CCheckQueue<T> * const pqueueIn) : pqueue(pqueueIn), fDone(false)
+    explicit CCheckQueueControl(CCheckQueue<T>& queueIn) : m_queue(queueIn), fDone(false)
     {
-        // passed queue is supposed to be unused, or nullptr
-        if (pqueue != nullptr) {
-            ENTER_CRITICAL_SECTION(pqueue->m_control_mutex);
-        }
+        ENTER_CRITICAL_SECTION(m_queue.m_control_mutex);
     }
 
     std::optional<R> Complete()
     {
-        if (pqueue == nullptr) return std::nullopt;
-        auto ret = pqueue->Complete();
+        auto ret = m_queue.Complete();
         fDone = true;
         return ret;
     }
 
     void Add(std::vector<T>&& vChecks)
     {
-        if (pqueue != nullptr) {
-            pqueue->Add(std::move(vChecks));
-        }
+        m_queue.Add(std::move(vChecks));
     }
 
     ~CCheckQueueControl()
     {
         if (!fDone)
             Complete();
-        if (pqueue != nullptr) {
-            LEAVE_CRITICAL_SECTION(pqueue->m_control_mutex);
-        }
+        LEAVE_CRITICAL_SECTION(m_queue.m_control_mutex);
     }
 };
 