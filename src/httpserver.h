pinheadmz/bitcoin

HTTPServer: generate sequential Ids for each newly accepted connection
Co-authored-by: Vasil Dimov <vd@FreeBSD.org>

src/httpserver.h
@@ -5,6 +5,7 @@
 #ifndef BITCOIN_HTTPSERVER_H
 #define BITCOIN_HTTPSERVER_H
 
+#include <atomic>
 #include <functional>
 #include <map>
 #include <memory>
@@ -287,6 +288,11 @@ class HTTPServer
 {
 public:
     /**
+     * Each connection is assigned an unique id of this type.
+     */
+    using Id = int64_t;
+
+    /**
      * Bind to a new address:port, start listening and add the listen socket to `m_listen`.
      * @param[in] to Where to bind.
      * @returns {} or the reason for failure.
@@ -320,12 +326,22 @@ private:
     std::vector<std::shared_ptr<Sock>> m_listen;
 
     /**
+     * The id to assign to the next created connection.
+     */
+    std::atomic<Id> m_next_id{0};
+
+    /**
      * Accept a connection.
      * @param[in] listen_sock Socket on which to accept the connection.
      * @param[out] addr Address of the peer that was accepted.
      * @return Newly created socket for the accepted connection.
      */
     std::unique_ptr<Sock> AcceptConnection(const Sock& listen_sock, CService& addr);
+
+    /**
+     * Generate an id for a newly created connection.
+     */
+    Id GetNewId();
 };
 } // namespace http_bitcoin
 

Kino1994/bitcoin-svn-git-history

src/httpserver.h
@@ -5,6 +5,7 @@
 #ifndef BITCOIN_HTTPSERVER_H
 #define BITCOIN_HTTPSERVER_H
 
+#include <atomic>
 #include <functional>
 #include <memory>
 #include <optional>
@@ -307,6 +308,11 @@ class HTTPServer
 {
 public:
     /**
+     * Each connection is assigned an unique id of this type.
+     */
+    using Id = uint64_t;
+
+    /**
      * Bind to a new address:port, start listening and add the listen socket to `m_listen`.
      * @param[in] to Where to bind.
      * @returns {} or the reason for failure.
@@ -340,12 +346,22 @@ private:
     std::vector<std::shared_ptr<Sock>> m_listen;
 
     /**
+     * The id to assign to the next created connection.
+     */
+    std::atomic<Id> m_next_id{0};
+
+    /**
      * Accept a connection.
      * @param[in] listen_sock Socket on which to accept the connection.
      * @param[out] addr Address of the peer that was accepted.
      * @return Newly created socket for the accepted connection.
      */
     std::unique_ptr<Sock> AcceptConnection(const Sock& listen_sock, CService& addr);
+
+    /**
+     * Generate an id for a newly created connection.
+     */
+    Id GetNewId();
 };
 } // namespace http_bitcoin
 